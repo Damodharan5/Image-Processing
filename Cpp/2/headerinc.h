13192262275963536d0afde33baa3b5c69388f5e
Damodharan Chandrasekaran <damolovestiger@ymail.com>
2017-09-06 16:43:42 -0500

    Add files via upload

---
+++ Cpp/2/headerinc.h
@@ -0,0 +1,13 @@
+#ifndef HEADERINC_H
+#define HEADERINC_H
+
+#include <iostream>
+#include<stdlib.h>
+#include<string>
+#include<conio.h>
+#include "bmpstruct.h"
+#include <ctime>
+#include <cuda.h>
+
+
+#endif
